03e79c830a646830bce5ba6baa9a747669e18209
VadimSurits <vadimsurits@gmail.com>
2021-05-09 14:30:57 +1000

    ДЗ 6 выполнено (не полностью): Добавьте в своё приложение экран со списком дел, в котором пользователь сможет создавать заметки, редактировать их или перемещать относительно друг друга. Примените для этого полученные на уроке знания.

---
+++ app/src/main/java/com/example/materialdesign_pictureoftheday/ui/recycler_activity/OnListItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.materialdesign_pictureoftheday.ui.recycler_activity
+
+import com.example.materialdesign_pictureoftheday.ui.recycler_activity.model.Data
+
+interface OnListItemClickListener {
+    fun onItemClick(data: Data)
+}
